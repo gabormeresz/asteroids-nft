fbe0254f5f92ab7a2f20c77700b82560f16b85bb
Gabor Meresz <gabormeresz84@gmail.com>
2024-01-26 22:52:38 +0100

    Cleaning up after abandoning app router and implemented logic in pages router

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,1 @@
 export { getMintedTokenId } from "./get-minted-tokenid";
-export { revalidateItem } from "./revalidate-item";
-export { revalidateCollection } from "./revalidate-collection";
--- src/actions/revalidate-item.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { revalidatePath } from "next/cache";
-
-export const revalidateItem = (tokenId: number) => {
-  console.log(`item ${tokenId} revalidated`);
-  revalidatePath(`/collection/${tokenId.toString()}`);
-};
