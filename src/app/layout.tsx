f08ed5fb0ed31b9b12a7db2d11df0d6f33186b5b
Gabor Meresz <gabormeresz84@gmail.com>
2024-01-28 21:09:51 +0100

    style change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: Props) {
       <Web3ModalProvider>
         {/* <body className="bg-[#1E2631] sm:bg-[url('../../public/background.jpg')] bg-cover bg-fixed bg-center h-screen h-full overflow-auto"> */}
         <body className=" h-screen h-full overflow-auto">
-          <div className="-z-10 fixed bg-cover bg-fixed bg-center bg-[url('../../public/background.jpg')] w-full h-full left-0 top-0"></div>
+          <div className="-z-10 fixed bg-cover bg-fixed bg-center bg-[url('../../public/background.jpg')] w-full h-screen left-0 top-0"></div>
           <div className="h-full flex flex-col justify-between gap-4">
             <Header />
             {children}
