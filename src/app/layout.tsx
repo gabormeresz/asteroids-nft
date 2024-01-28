c5d1393df45a5ed9349842a6045dfd8067caa454
Gabor Meresz <gabormeresz84@gmail.com>
2024-01-28 20:57:14 +0100

    attempt to make background look properly on ios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,9 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <Web3ModalProvider>
-        <body
-          style={{
-            backgroundImage: `url(${backgroundImage.src})`
-          }}
-          className="bg-cover bg-fixed bg-center h-screen h-full overflow-auto"
-        >
+        {/* <body className="bg-[#1E2631] sm:bg-[url('../../public/background.jpg')] bg-cover bg-fixed bg-center h-screen h-full overflow-auto"> */}
+        <body className=" h-screen h-full overflow-auto">
+          <div className="-z-10 fixed bg-cover bg-fixed bg-center bg-[url('../../public/background.jpg')] w-full h-full left-0 top-0"></div>
           <div className="h-full flex flex-col justify-between gap-4">
             <Header />
             {children}
